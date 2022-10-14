01341b939be8c0334ed941bf326055e3f5ef3a10
Swattyoungboy <103699206+Swattyoungboy@users.noreply.github.com>
2022-10-14 19:30:44 +0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -153,3 +153,23 @@ window.onscroll = () =>{
   });
 
 })(jQuery);
+
+
+
+
+
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+var len = acc.length;
+for (i = 0; i < len; i++) {
+  acc[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+}
